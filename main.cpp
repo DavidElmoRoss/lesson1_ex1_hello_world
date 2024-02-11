6b55c77de4f39341fa3874a33be899cb8749c2f3
David Elmo Ross <110311948+DavidElmoRoss@users.noreply.github.com>
2024-02-11 13:39:49 -0500

    Update main.cpp
    Added comments

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ This program uses printf to send Hello World to the Screen
 // main() runs in its own thread in the OS
 int main()
 {
-   printf("Hello World\n");
-   for(;;);
+   printf("Hello World\n");              // the  \n  puts cursor on Next Line
+   for(;;);                              // must stop program from going past last } 
 }
 
